25560078dac69436b7a95b33c34c2738cd4f3fc7
Kapone <alkapone.public@gmail.com>
2021-08-01 17:07:29 -0400

    Removed node-fetch and added axios

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,1 @@
-import { IMeme } from './lib';
-/**
- *
- * @param url The name of the subreddit
- * @param force If we want to force the url to be an image
- * @returns all the necessary information of the post
- */
-export declare function getMeme(url: string, force?: boolean): Promise<IMeme>;
+export * from "./lib";
